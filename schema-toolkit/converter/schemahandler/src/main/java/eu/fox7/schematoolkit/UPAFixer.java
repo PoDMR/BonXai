c624eac5bda976141b621fe32ca5a0ce9b48c6d7
niewerth <niewerth@cab26698-93e7-4b1a-9fb9-bd068492429d>
2012-02-23 13:17:18 +0000

    Added UPAFixer interface to SchemaHandler.
    git-svn-id: https://nirwana.cs.uni-dortmund.de/repos/FoXLib@331 cab26698-93e7-4b1a-9fb9-bd068492429d

---
+++ schema-toolkit/converter/schemahandler/src/main/java/eu/fox7/schematoolkit/UPAFixer.java
@@ -0,0 +1,5 @@
+package eu.fox7.schematoolkit;
+
+public interface UPAFixer {
+	public void fixUPA(Schema schema) throws SchemaToolkitException;
+}
--- schema-toolkit/converter/schemahandler/src/main/java/eu/fox7/schematoolkit/exceptions/ConversionFailedException.java
+++ schema-toolkit/converter/schemahandler/src/main/java/eu/fox7/schematoolkit/exceptions/ConversionFailedException.java
@@ -2,6 +2,11 @@ package eu.fox7.schematoolkit.exceptions;
 
 public class ConversionFailedException extends Exception {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public ConversionFailedException() {
 	}
 
