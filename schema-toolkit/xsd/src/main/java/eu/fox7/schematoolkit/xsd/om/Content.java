475b4bdca96ae71fb2dcaddef896274cc253917b
niewerth <niewerth@cab26698-93e7-4b1a-9fb9-bd068492429d>
2012-11-16 08:32:48 +0000

    Added copyright notices for schema-toolkit.
    git-svn-id: https://nirwana.cs.uni-dortmund.de/repos/FoXLib@465 cab26698-93e7-4b1a-9fb9-bd068492429d

--- schema-toolkit/xsd/src/main/java/eu/fox7/schematoolkit/xsd/om/Content.java
+++ schema-toolkit/xsd/src/main/java/eu/fox7/schematoolkit/xsd/om/Content.java
@@ -1,12 +1,14 @@
 /**
- * This file is part of BonXai.
+ * Copyright 2009-2012 TU Dortmund
  *
- * BonXai is free software: you can redistribute it and/or modify
+ * This file is part of FoXLib.
+ *
+ * FoXLib is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * BonXai is distributed in the hope that it will be useful,
+ * FoXLib is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
@@ -14,6 +16,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with BonXai.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package eu.fox7.schematoolkit.xsd.om;
 
 import eu.fox7.schematoolkit.common.AnnotationElement;
--- schema-toolkit/xsd/src/main/java/eu/fox7/schematoolkit/xsd/parser/exceptions/UnknownNamespaceException.java
+++ schema-toolkit/xsd/src/main/java/eu/fox7/schematoolkit/xsd/parser/exceptions/UnknownNamespaceException.java
@@ -1,7 +1,22 @@
 /**
- * This Exception is thrown when a namespace is used that
- * is not know in the schema's list of namespaces.
+ * Copyright 2009-2012 TU Dortmund
+ *
+ * This file is part of FoXLib.
+ *
+ * FoXLib is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * FoXLib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with BonXai.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package eu.fox7.schematoolkit.xsd.parser.exceptions;
 
 public class UnknownNamespaceException extends eu.fox7.schematoolkit.xsd.parser.exceptions.XSDParseException {
